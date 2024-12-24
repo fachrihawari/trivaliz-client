cc9f958ba23a69e07407994672dfe94d107b8f45
Fachri Hawari <fachri.hawari@gmail.com>
2024-12-24 11:28:47 +0700

    move the page to login after finish intro

--- src/app/(main)/intro/page.tsx
+++ src/app/(main)/intro/page.tsx
@@ -1,16 +1,20 @@
 'use client'
 
 import { markIntroShown } from "@/actions/cookies"
+import { useRouter } from "next/navigation"
 
 export default function IntroPage() {
+  const router = useRouter()
+
   return (
     <div>
-      <h1 className="text-2xl">Intro</h1>
+      <h1 className="text-2xl">Intro Page</h1>
+      <p>coming soon!</p>
 
-      <button onClick={() => {
+      <button onClick={async () => {
         // console.log('marking intro shown')
-        markIntroShown()
-          .catch(console.log)
+        await markIntroShown()
+        router.push('/login')
       }}>go to Intro</button>
     </div>
   )
